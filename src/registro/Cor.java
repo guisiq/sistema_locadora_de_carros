d43482b4fc285f052b816ed4e4d38a2993473610
guisiq <lopesgui01@hotmail.com>
2020-12-03 23:42:27 -0300

    inprementando  metodos em cor

--- src/registro/Cor.java
+++ src/registro/Cor.java
@@ -3,9 +3,26 @@ package registro;
 public enum Cor {
     PRETO("#000000"), PRATA("#bab8b5"), BRANCO("ffffff"), VERMELHO("ff0000");
 
-    String rgbCode;
+    private String rgbCode;
 
     Cor(String rgbCode) {
         this.rgbCode = rgbCode;
     }
+
+    public Cor valueOf(int i ){
+        if(i > Cor.values().length){
+            return Cor.values()[i];
+        }
+        return null ;
+    }
+
+    // #region metodos getters e setters
+    public String getRgbCode() {
+        return rgbCode;
+    }
+
+    public void setRgbCode(String rgbCode) {
+        this.rgbCode = rgbCode;
+    }
+    // #endregion
 }
