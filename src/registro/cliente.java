aed986350b5df4d4f69bab8cb70bcf1135bcab16
Debora Ribeiro Pereira <deboraribeiro9015@gmail.com>
2020-12-04 18:43:19 -0300

    update na classe Cliente
    métodos acessores

--- src/registro/cliente.java
+++ src/registro/cliente.java
@@ -1,9 +1,49 @@
 package registro;
 
-public class cliente {
+public class Cliente {
     private String id;
     private String nome;
     private String cpf;
     private String email;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+	public Cliente(String id, String nome, String cpf, String email) {
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+	}
+
+    
 }
