be115c6f3bc6030dc3328577a7807564da84c4c1
Debora Ribeiro Pereira <deboraribeiro9015@gmail.com>
2020-12-05 20:32:53 -0300

    Update registro.java
    arrays list - adequação

--- src/Registro.java
+++ src/Registro.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 //import jdk.nashorn.internal.parser.DateParser;
 import registro.*;
+
 public abstract class Registro {
     public static List<Carro>   lCarros   = new ArrayList<Carro>(Arrays.asList(new Carro[]{
         new Carro(1, "KDH-7821", new Date(2005, 10, 5) , 2002, "Hilux CS DX 4x2 2.7 16V 142cv", Cor.AZUL,Categoria.INTERMEDIÁRIO),
@@ -13,7 +14,8 @@ public abstract class Registro {
         new Carro(4, "MNG1163", new Date(2005,10,5) ,1994, "Espero 2.0", Cor.MARROM,Categoria.POPULAR),
         new Carro(5, "MZS9310", new Date(2005,10,5) ,2008, "MARRU", Cor.PRATA,Categoria.INTERMEDIÁRIO),
     }));
-    public static List<Cliente> lClientes = new ArrayList<Cliente>(Arrays.asList(new Cliente[]{
+
+public static List<Cliente> lClientes = new ArrayList<Cliente>(Arrays.asList(new Cliente[]{
         new Cliente(1, "Thales Mateus Kaique Cardoso", "07794037891","thalesmateuskaiquecardoso..thalesmateuskaiquecardoso@outloock.com"),
         new Cliente(2, "Luan Matheus João Barros", "49006244627","lluanmatheusjoaobarros@wizardsjc.com.br"),
         new Cliente(3, "Débora Benedita Elisa da Rocha", "11183004249","ddeborabeneditaelisadarocha@modus.com.br"),
@@ -23,10 +25,7 @@ public abstract class Registro {
         new Cliente(1, "Yuri Luan Fernandes", "82162188937","yuriluanfernandes..yuriluanfernandes@lonax.net"),
         new Cliente(1, "Alice Maria Porto", "46537485606","alicemariaporto..alicemariaporto@compecia.com.br")
     }));
-    //public static List<Locacao> lLocacoes = new ArrayList<Locacao>(Arrays.asList(new Cliente[]{
-        //new LocacaoDiaria();
-
-    //}));
+ 
     public static void init(){
     }
 }
