379e86e85a4779fce851e57a1af3cb84041703bf
Debora Ribeiro Pereira <deboraribeiro9015@gmail.com>
2020-12-06 17:07:18 -0300

    Update Registro.java
    Implementando lista de locações

--- src/Registro.java
+++ src/Registro.java
@@ -27,10 +27,10 @@ public abstract class Registro {
         new Cliente(1, "Alice Maria Porto", "46537485606","alicemariaporto..alicemariaporto@compecia.com.br")*/
     }));
     public static List<Locacao> lLocacoes = new ArrayList<Locacao>(Arrays.asList(new Locacao[]{
-        //new LocacaoDiaria();
-        new LocacaoDiaria(),
-        null,
-        null
+        new Locacao(2,"05/04/2012","06/09/2012",3.000,97.000,new Carro(1, "KDH-7821", new Date(2006, 12, 5) , 2002, "Hilux CS DX 4x2 2.7 16V 142cv", Cor.AZUL,Categoria.INTERMEDIÁRIO)),
+        new Locacao(3,"06/08/2012","12/09/2012",2.800,60.000, new Carro(2, "KEQ-0424", new Date(2009, 10, 7) , 1993, "Corolla DX/ SW DX 1.6 16V", Cor.LARANJA,Categoria.POPULAR)),
+        new Locacao(4,"16/08/2012","24/09/2012",1.800,69.000, new Carro(3, "KIO-0964", new Date(2004, 10, 7) , 1993, "Corolla DX/ SW DX 1.6 16V", Cor.AZUL,Categoria.POPULAR)),
+        new Locacao(4,"16/08/2012","24/09/2012",1.800,69.000, new Carro(4, "MNG1163", new Date(2010,06,5) ,1994, "Espero 2.0", Cor.MARROM,Categoria.POPULAR)),
     }));
     public static Usuario adm  = new Usuario("root", "admlogin", Usuario.Acesso.ADM);
 }
