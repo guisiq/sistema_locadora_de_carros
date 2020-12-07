4d88add639c4994d93491b85040092d7b2c6cffa
guisiq <lopesgui01@hotmail.com>
2020-12-07 13:13:46 -0300

    ajustando os registros

--- src/Registro.java
+++ src/Registro.java
@@ -9,11 +9,11 @@ import registro.*;
 public abstract class Registro {
 
     public static List<Carro>   lCarros   = new ArrayList<Carro>(Arrays.asList(new Carro[]{
-        new Carro(1, "KDH-7821", new Date(2005, 10, 5) , 2002, "Hilux CS DX 4x2 2.7 16V 142cv", Cor.AZUL,Categoria.INTERMEDIÁRIO),
-        new Carro(2, "KEQ-0424", new Date(2005, 10, 5) , 1993, "Corolla DX/ SW DX 1.6 16V", Cor.LARANJA,Categoria.POPULAR),
-        new Carro(3, "NBC6843", new Date(2005,10,5) , 1991, "Camper GL/GLS 4x4 4.0 Diesel", Cor.MARROM,Categoria.INTERMEDIÁRIO),
-        new Carro(4, "MNG1163", new Date(2005,10,5) ,1994, "Espero 2.0", Cor.MARROM,Categoria.POPULAR),
-        new Carro(5, "MZS9310", new Date(2005,10,5) ,2008, "MARRU", Cor.PRATA,Categoria.INTERMEDIÁRIO),
+        new Carro("KDH-7821", new Date(2005, 10, 5) , 2002, "Hilux CS DX 4x2 2.7 16V 142cv", Cor.AZUL,Categoria.INTERMEDIÁRIO),
+        new Carro("KEQ-0424", new Date(2005, 10, 5) , 1993, "Corolla DX/ SW DX 1.6 16V", Cor.LARANJA,Categoria.POPULAR),
+        new Carro("NBC6843" , new Date(2005,10,5)   , 1991, "Camper GL/GLS 4x4 4.0 Diesel", Cor.MARROM,Categoria.INTERMEDIÁRIO),
+        new Carro("MNG1163" , new Date(2005,10,5)   , 1994, "Espero 2.0", Cor.MARROM,Categoria.POPULAR),
+        new Carro("MZS9310" , new Date(2005,10,5)   , 2008, "MARRU", Cor.PRATA,Categoria.INTERMEDIÁRIO),
     }));
 
     public static List<Cliente> lClientes = new ArrayList<Cliente>(Arrays.asList(new Cliente[]{
@@ -27,11 +27,11 @@ public abstract class Registro {
         new Cliente(8, "Alice Maria Porto", "46537485606","alicemariaporto..alicemariaporto@compecia.com.br")*/
     }));
     public static List<Locacao> lLocacoes = new ArrayList<Locacao>(Arrays.asList(new Locacao []{
-        new LocacaoDiaria(new Date(2012,05,04),new Date(2012,06,09),3.000f,Registro.lCarros.get(1),10),
-        new LocacaoDiaria(new Date(2012,06,08),new Date(2012,12,09),2.800f,Registro.lCarros.get(2),10),
-        new LocacaoDiaria(new Date(2012,16,08),new Date(2012,24,09),1.800f,Registro.lCarros.get(3),10),
-        new LocacaoDiaria(new Date(2012,16,08),new Date(2012,24,09),1.800f,Registro.lCarros.get(4),10),
-        new LocacaoDiaria(new Date(2012,16,08),new Date(2012,24,09),1.800f,Registro.lCarros.get(4),10)
+        new LocacaoDiaria(ConsoleManager.lerdDate("04/05/2012"),ConsoleManager.lerdDate("09/06/2012"),3.000f,Registro.lCarros.get(1),10),
+        new LocacaoDiaria(ConsoleManager.lerdDate("08/06/2012"),ConsoleManager.lerdDate("09/12/2012"),2.800f,Registro.lCarros.get(2),10),
+        new LocacaoDiaria(ConsoleManager.lerdDate("08/16/2012"),ConsoleManager.lerdDate("09/24/2012"),1.800f,Registro.lCarros.get(3),10),
+        new LocacaoDiaria(ConsoleManager.lerdDate("08/16/2012"),ConsoleManager.lerdDate("09/24/2012"),1.800f,Registro.lCarros.get(4),10),
+        new LocacaoDiaria(ConsoleManager.lerdDate("08/16/2012"),ConsoleManager.lerdDate("09/24/2012"),1.800f,Registro.lCarros.get(4),10)
     }));
     public static Usuario adm  = new Usuario("root", "admlogin", Usuario.Acesso.ADM);
 }
